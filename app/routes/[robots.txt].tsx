abf8faf7a4d7c5f3bb25586a24a82cb0a9cd3dc9
Yehor Iliushchenko <buaueru@gmail.com>
2023-04-05 10:39:58 +0300

    Allow all robot

--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -3,45 +3,51 @@ import { DOMAIN } from '~/shared/const'
 export const loader = () => {
   // handle "GET" request
   // set up our text content that will be returned in the response
+  //   const robotText = `
+  //   User-agent: Applebot
+  // Allow: /
+
+  // User-agent: baiduspider
+  // Allow: /
+
+  // User-agent: Bingbot
+  // Allow: /
+
+  // User-agent: Facebot
+  // Allow: /
+
+  // User-agent: Googlebot
+  // Allow: /
+
+  // User-agent: msnbot
+  // Allow: /
+
+  // User-agent: Naverbot
+  // Allow: /
+
+  // User-agent: seznambot
+  // Allow: /
+
+  // User-agent: Slurp
+  // Allow: /
+
+  // User-agent: teoma
+  // Allow: /
+
+  // User-agent: Twitterbot
+  // Allow: /
+
+  // User-agent: Yeti
+  // Allow: /
+
+  // User-agent: *
+  // Disallow: /
+
+  // Sitemap: ${DOMAIN}/sitemap.xml`
+
   const robotText = `
-  User-agent: Applebot
-Allow: /
-
-User-agent: baiduspider
-Allow: /
-
-User-agent: Bingbot
-Allow: /
-
-User-agent: Facebot
-Allow: /
-
-User-agent: Googlebot
-Allow: /
-
-User-agent: msnbot
-Allow: /
-
-User-agent: Naverbot
-Allow: /
-
-User-agent: seznambot
-Allow: /
-
-User-agent: Slurp
-Allow: /
-
-User-agent: teoma
-Allow: /
-
-User-agent: Twitterbot
-Allow: /
-
-User-agent: Yeti
-Allow: /
-
 User-agent: *
-Disallow: /
+Allow: /
 
 Sitemap: ${DOMAIN}/sitemap.xml`
   // return the text content, a status 200 success response, and set the content type to text/plain
